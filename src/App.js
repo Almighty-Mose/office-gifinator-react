97c2910194534ebf9e361d17299ccfbf986238ae
Branden <branden.mosley1337@gmail.com>
2019-04-20 00:13:46 -0700

    Undo last commit

--- src/App.js
+++ src/App.js
@@ -7,23 +7,6 @@ import './App.css';
 
 const StyledApp = styled.div`
   text-align: center;
-  /* scrollbar - NOT SUPPORTED IN IE/EDGE OR FIREFOX */
-
-  /* width */
-  ::-webkit-scrollbar {
-    width: 10px;
-  }
-
-  /* Handle */
-  ::-webkit-scrollbar-thumb {
-    background: #1b1b1b; 
-    border-radius: 5px;
-  }
-
-  /* Handle on hover */
-  ::-webkit-scrollbar-thumb:hover {
-    background: gray; 
-}
 `
 
 const App = () => {
