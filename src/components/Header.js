501620a5d398d5809cd18079fa0e885a33886b34
Branden <branden.mosley1337@gmail.com>
2019-08-06 13:50:52 -0700

    remove unnecessary CSS rules from StyledHeader component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledHeader = styled.header`
-    margin: 0;
+    margin: 0 auto;
+    padding: 20px;
     background-color: #282c34;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
     color: white;
     height: 100px;
     font-size: 2em;
